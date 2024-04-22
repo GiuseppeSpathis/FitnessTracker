6331305c32d53c5ece0f224aa27ca419fb4dfeee
Giuseppe <giuseppspathis@gmail.com>
2024-04-22 12:54:36 +0200

    aggiunta la libreria dei toast success e not

--- app/src/main/java/com/example/fitnesstracker/MyAdapter.kt
+++ app/src/main/java/com/example/fitnesstracker/MyAdapter.kt
@@ -13,6 +13,17 @@ import com.example.fitnesstracker.Person
 import com.example.fitnesstracker.PersonProfile
 import com.example.fitnesstracker.R
 import Utils.checkGender
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.core.content.res.ResourcesCompat
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
 class MyAdapter(private var personList: List<Person>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
@@ -41,8 +52,7 @@ class MyAdapter(private var personList: List<Person>) : RecyclerView.Adapter<MyA
             it.context.startActivity(intent)
         }
         holder.message.setOnClickListener{
-            Toast.makeText(it.context, "vuoi messaggiare", Toast.LENGTH_SHORT).show() //da modificare in futuro
-
+            showDialog(it.context, holder.name.text.toString())
         }
         holder.share.setOnClickListener{
             Toast.makeText(it.context, "vuoi condividere", Toast.LENGTH_SHORT).show() //da modificare in futuro
@@ -57,4 +67,68 @@ class MyAdapter(private var personList: List<Person>) : RecyclerView.Adapter<MyA
         personList = newList
         notifyDataSetChanged()
     }
+
+    private fun showDialog(context: Context, name: String) {
+        val builder = AlertDialog.Builder(context, R.style.DialogTheme)
+        builder.setTitle("Invia un toast a $name")
+
+        val input = EditText(context)
+        val layout = LinearLayout(context)
+        layout.setPadding(40, 40, 40, 40)
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        input.layoutParams = params
+        layout.addView(input)
+        builder.setView(layout)
+
+        // Creo un OnClickListener vuoto perche' il dialog di base si chiude appena clicchi uno dei due pulsanti
+        //indipendentemente dal onClickListener del pulsante che in questo caso nel pulsante invia devi fare il check se
+        //l'input text sia vuoto oppure no
+        val dummyListener = DialogInterface.OnClickListener { _, _ -> }
+
+        builder.setPositiveButton("Invia", dummyListener)
+
+        builder.setNegativeButton("Annulla") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        val dialog = builder.create()
+        dialog.setOnShowListener {
+            val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            positiveButton.isEnabled = false // all'inizio il pulsante e' disabilitato
+
+            // Imposta il vero OnClickListener sul pulsante positivo
+            positiveButton.setOnClickListener {
+                val toastMessage = input.text.toString() //in futuro questo toastMessage devi inviare un toast a quell'utente
+
+                dialog.dismiss()
+
+                if(context is Activity){
+                    MotionToast.createColorToast(context,
+                        "Successo",
+                        "il toast é stato inviato con successo",
+                        MotionToastStyle.SUCCESS,
+                        MotionToast.GRAVITY_BOTTOM,
+                        MotionToast.LONG_DURATION,
+                        ResourcesCompat.getFont(context, www.sanju.motiontoast.R.font.helvetica_regular))
+                }
+            }
+
+            input.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable) {
+                    positiveButton.isEnabled = s.isNotEmpty() // il pulsante diventa abilitato se il testo non e' vuoto
+                }
+
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            })
+        }
+
+        dialog.show()
+    }
+
+
 }
